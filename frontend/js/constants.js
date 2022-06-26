21d3649d405864b9f08d6a153c867403408e551b
broekingc <broekingc@gmail.com>
2022-06-26 17:40:55 -0500

    rinkby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x34695a62420637FEdFaC2f713b75a80E57dE60a0"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
